1e9806e6ffb8e6729cc6d851d873abd2f7dbd36d
Saurabh Dutta <duttasaurabh92@gmail.com>
2021-09-29 08:40:01 +0000

    js update

--- index.js
+++ index.js
@@ -42,9 +42,17 @@ const questionList = [
     question: "Who's my favorite superhero?",
     answer: "Shaktiman"
   },
+  {
+    question: "Which city i lived before 2020?",
+    answer: "Bangalore"
+  },
 {
     question: "What is my favourite dish?",
     answer: "Indian"
+  },
+  {
+    question: "What is the nickname given by my grandfather?",
+    answer: "Dhrubo"
   }
 
 ];
@@ -63,9 +71,9 @@ while (i < questionList.length) {
 //top scores
 
 var topScores = [
-  { name: "Saurabh", score: 3 },
-  { name: "Sameera", score: 3 },
-  { name: "Saptarshi", score: 2 }
+  { name: "Saurabh", score: 5 },
+  { name: "Sameera", score: 4 },
+  { name: "Saptarshi", score: 5 }
 ];
 
 console.log("These are the top scores, if you have more than these, please send me a screenshot!!")
